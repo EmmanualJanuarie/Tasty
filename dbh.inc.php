4a77ba6d19504875f7538e0e79a28e4081e62c01
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2023-12-07 23:28:19 +0200

    Update the dbh file
    u

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,17 +1,19 @@
 <!--===================== [Connecting to MySql Database ] ==========================-->
-<php?
+<?php
  
-$dsn ="mysql:host=localhost; dbname=tasty_db;"
+$dsn ="mysql:host=localhost; dbname=tasty_db;";
 
-$dbusername ="root";
-$dbpassword ="";
+$dbusername="root";
+$dbpassword= "";
 
-try{
-    $pdo = new PDO($dsn, $dbusername, $dbpassword );
-    $pdo->setAttribute(PDO::)
-}catch () {
-
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword); //This will allow us to connect to the database
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//will allow us to cahnge some features of the 'pdo' object 
+} catch (PDOException $e) {
+   echo "Connection Failed". $e->getMessage();
 }
+
+?>
 
 
 
