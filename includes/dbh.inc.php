0bafd8875dc4daf3e8c441da838df7da7db45e6e
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2023-12-10 17:29:18 +0200

    cHANGED SOME THINGS

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,16 @@
 <!--===================== [Connecting to MySql Database ] ==========================-->
 <?php
  
-$dsn ="mysql:host=localhost; dbname=tasty_db;";
+$dsn = "mysql:host=localhost; dbname=tasty_db";
 
-$dbusername="root";
+$dbusername = "root";
 $dbpassword= "";
 
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword); //This will allow us to connect to the database
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//will allow us to cahnge some features of the 'pdo' object 
 } catch (PDOException $e) {
-   echo "Connection Failed". $e->getMessage();
+   echo "Connection Failed: ". $e->getMessage();
 }
 
 
--- includes/formhandler.inc.php
+++ includes/formhandler.inc.php
@@ -1,42 +1,41 @@
 <!-- The script allows data that has beenm entered in the "signUp form". To be transfered and stored into the database -->
 <?php
     // Check if user enters the signUp page correctly 
-    if($_SERVER["REQUEST_METHOD"] == "post"){
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
 
         // Grabbing data from form to sent to database
         $username = $_POST["username"];
-        $pwd = $_POST["password"];
+        $pwd = $_POST["pwd"];
         $email = $_POST["email"];
 
-    //  Ruuning a try-catch statement
-    try {
-        // Grabbing connection to database
-        require_once "dbh.inc.php"; //Allows access to another php file, or link to a php file
+        //  Ruuning a try-catch statement
+        try {
+            // Grabbing connection to database
+            require_once "dbh.inc.php"; //Allows access to another php file, or link to a php file
 
-        $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);"; // (?) acts as a placeholder for the Values
+            $query = "INSERT INTO users (username, pwd, email) VALUES (?, ?, ?);"; // (?) acts as a placeholder for the Values
 
-        // Fabricating a prepared statement to query the data inside the database
-        $statement = $pdo->prepare($query);
+            // Fabricating a prepared statement to query the data inside the database
+            $stmnt = $pdo->prepare($query);
 
-        // executing the values from the array
-        $statement->execute([$username, $pwd, $email]);
+            // executing the values from the array
+            $stmnt->execute([$username, $pwd, $email]);
 
-        // removing any data from the statement and query to free up resources
-        $query = null;
-        $statement = null;
+            // removing any data from the statement and pdo to free up resources
+            $pdo = null;
+            $stmnt = null;
 
-        // Sending user back to menu wants they Sign Up
-        header("Location: ../index.html"); 
-        
-        die();
+            // Sending user back to menu wants they Sign Up
+            header("Location: ../index.html"); 
+            
+            die();
 
 
-    } catch (PDOException $e) {
-        // Output a error message
-        die("Query Failed: " . $e->getMessage());
-    }
+        } catch (PDOException $e) {
+            // Output a error message
+            die("Query Failed: " . $e->getMessage());
+        }
 
     }else{ 
         header("Location: ../index.html"); //This header, accompanied by the Location attribute will send the user back to the main menu.
-    }
-    // Check if the user enters data into the Form
+    };
